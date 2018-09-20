89ef4527a26af3ef91c51147efe009216e2ee979
yogesh <yogeshduriya@gmail.com>
2018-09-20 19:55:32 +0530

    google Auto places

--- app/src/main/java/com/kirana/ui/deliveryDetails/view/DeliveryDetailsActivity.kt
+++ app/src/main/java/com/kirana/ui/deliveryDetails/view/DeliveryDetailsActivity.kt
@@ -1,119 +1,136 @@
 package com.kirana.ui.deliveryDetails.view
 
+import android.content.Context
+import android.location.Location
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import android.os.Handler
-import android.os.HandlerThread
-import android.os.Message
-import android.os.Process
 import android.text.Editable
 import android.text.TextWatcher
-import android.view.View
 import com.kirana.R
 import kotlinx.android.synthetic.main.activity_delivery_details.*
 import android.widget.Toast
-import android.widget.AdapterView
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.LatLngBounds
+import com.google.android.gms.location.places.Places
+import android.util.Log
+import android.view.View
+import android.view.inputmethod.InputMethodManager
+import com.google.android.gms.location.*
+import com.google.android.gms.location.places.GeoDataClient
+import com.google.android.gms.location.places.PlaceBufferResponse
+import com.google.android.gms.tasks.OnCompleteListener
+import com.google.android.gms.tasks.Task
 
 
 class DeliveryDetailsActivity : AppCompatActivity() {
     private val TAG = DeliveryDetailsActivity::class.java!!.getName()
-    private var mAdapter: PlacesAutoCompleteAdapter? = null
-    lateinit var mHandlerThread: HandlerThread
-    var mThreadHandler: Handler? = null
-    //lateinit var dataAdapter: GooglePlacesAutompleteAdapter
+    lateinit var placesAdapter: PlacesAdapter
+    lateinit var latLng: LatLng
+    lateinit var mLocationRequest: LocationRequest
+    lateinit var mFusedLocationClient: FusedLocationProviderClient
+    lateinit var mLocationCallback: LocationCallback
+    lateinit var mGeoDataClient: GeoDataClient
+    lateinit var mSettingsClient: SettingsClient
+    lateinit var mLocationSettingsRequest: LocationSettingsRequest
+    private val REQUEST_CHECK_SETTINGS = 0x1
+    var isAutoCompleteLocation = false
+    lateinit var location: Location
+    val REQUEST_LOCATION = 1011
+    var selectedLocation = ""
+    val BOUNDS_INDIA = LatLngBounds(LatLng(23.63936, 68.14712), LatLng(28.20453, 97.34466))
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_delivery_details)
 
-        mAdapter = PlacesAutoCompleteAdapter(this, R.layout.autocomplete_list_item)
-        mAdapter?.resultList?.add("footer")
-        autocomplete.setAdapter(mAdapter)
+        mGeoDataClient = Places.getGeoDataClient(this, null);
 
-        autocomplete.setOnItemClickListener(object : AdapterView.OnItemClickListener {
-            override fun onItemClick(parent: AdapterView<*>, view: View, position: Int, id: Long) {
-                // Get data associated with the specified position
-                // in the list (AdapterView)
-                val description = parent.getItemAtPosition(position) as String
-                showTost(description)
+        mLocationCallback = object : LocationCallback() {
+            override fun onLocationResult(locationResult: LocationResult?) {
+                val loc = locationResult!!.lastLocation
+                if(!isAutoCompleteLocation) {
+                    location = loc
+                    latLng = LatLng(location.latitude, location.longitude)
 
-            }
-        })
-
-        autocomplete.addTextChangedListener(object : TextWatcher {
-            override fun beforeTextChanged(s: CharSequence, start: Int, count: Int, after: Int) {
-
-            }
-
-            override fun onTextChanged(s: CharSequence, start: Int, before: Int, count: Int) {
-                val value = s.toString()
-
-                // Remove all callbacks and messages
-                mThreadHandler?.removeCallbacksAndMessages(null)
-
-                // Now add a new one
-                mThreadHandler?.postDelayed(Runnable {
-                    // Background thread
-
-                    mAdapter?.resultList = mAdapter?.mPlaceAPI?.autocomplete(value)!!
-
-                    // Footer
-                    if (mAdapter?.resultList!!.size > 0)
-                        mAdapter?.resultList?.add("footer")
-
-                    // Post to Main Thread
-                    mThreadHandler!!.sendEmptyMessage(1)
-                }, 500)
-            }
-
-            override fun afterTextChanged(s: Editable) {
-                //doAfterTextChanged()
-            }
-        })
-
-    }
-
-    private fun showTost(description: String) {
-        Toast.makeText(this, description, Toast.LENGTH_SHORT).show()
-    }
-
-    public override fun onDestroy() {
-        super.onDestroy()
-
-        // Get rid of our Place API Handlers
-        if (mThreadHandler != null) {
-            mThreadHandler!!.removeCallbacksAndMessages(null)
-            mHandlerThread.quit()
-        }
-    }
-
-
-
-    init{
-        // Required empty public constructor
-
-        if (mThreadHandler == null) {
-            // Initialize and start the HandlerThread
-            // which is basically a Thread with a Looper
-            // attached (hence a MessageQueue)
-            mHandlerThread = HandlerThread(TAG, Process.THREAD_PRIORITY_BACKGROUND)
-            mHandlerThread.start()
-
-            // Initialize the Handler
-            mThreadHandler = object : Handler(mHandlerThread.looper) {
-                override fun handleMessage(msg: Message) {
-                    if (msg.what === 1) {
-                        val results = mAdapter?.resultList
-
-                        if (results != null && results!!.size > 0) {
-                            mAdapter?.notifyDataSetChanged()
-                        } else {
-                            mAdapter?.notifyDataSetInvalidated()
-                        }
-                    }
                 }
             }
+
+        }
+        mLocationRequest = LocationRequest.create()
+                .setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY)
+                .setInterval((10 * 1000).toLong())        // 10 seconds, in milliseconds
+                .setFastestInterval((6 * 1000).toLong()) // 1 second, in milliseconds
+
+        mSettingsClient = LocationServices.getSettingsClient(this)
+        val builder = LocationSettingsRequest.Builder();
+        builder.addLocationRequest(mLocationRequest)
+        mLocationSettingsRequest = builder.build()
+
+        placesAdapter = PlacesAdapter(this, android.R.layout.simple_list_item_1, mGeoDataClient, null, BOUNDS_INDIA)
+        enter_place.setAdapter(placesAdapter)
+        enter_place.addTextChangedListener(object : TextWatcher {
+            override fun afterTextChanged(s: Editable?) {
+            }
+
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                if (count > 0) {
+                    cancel.visibility = View.VISIBLE
+                } else {
+                    cancel.visibility = View.GONE
+                }
+            }
+        })
+        enter_place.setOnItemClickListener({ parent, view, position, id ->
+            //getLatLong(placesAdapter.getPlace(position))
+            hideKeyboard()
+            val item = placesAdapter.getItem(position)
+            val placeId = item?.getPlaceId()
+            val primaryText : String = item?.getPrimaryText(null).toString()
+
+            enter_place.setText(primaryText + ", " + item?.getSecondaryText(null).toString())
+
+            Log.i("Autocomplete", "Autocomplete item selected: " + primaryText)
+
+
+            val placeResult = mGeoDataClient.getPlaceById(placeId)
+            placeResult.addOnCompleteListener(object : OnCompleteListener<PlaceBufferResponse> {
+                override fun onComplete(task: Task<PlaceBufferResponse>) {
+                    val places = task.getResult();
+                    val place = places.get(0)
+
+                    isAutoCompleteLocation = true
+                    latLng = place.latLng
+                    //assignToMap()
+
+                    places.release()
+                }
+
+            })
+
+            Toast.makeText(applicationContext, "Clicked: " + primaryText,
+                    Toast.LENGTH_SHORT).show()
+        })
+        cancel.setOnClickListener {
+            enter_place.setText("")
+        }
+    }
+
+
+
+    fun hideKeyboard() {
+        try {
+            val inputMethodManager = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+            inputMethodManager.hideSoftInputFromWindow(currentFocus!!.windowToken, 0)
+        } catch (e: Exception) {
+            e.printStackTrace()
         }
 
     }
+
+
+
 }
